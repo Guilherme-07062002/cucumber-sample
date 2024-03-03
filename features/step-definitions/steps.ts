dd820a27a6e35980accc024b70d3f8fd7b0e3d0e
Guilherme-07062002 <guilherme762002@gmail.com>
2024-03-03 14:43:06 -0300

    refactor: return "tgif" if today is "friday"

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -1,8 +1,9 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import assert from "assert";
 
+// TGIF - Thank God It's Friday
 const itIsFriday = (today: string) => {
-	return "Nope";
+	return today === "Friday" ? "TGIF" : "Nope";
 };
     
 Given("today is Sunday", function () {
@@ -14,5 +15,5 @@ When("I ask whether it's Friday yet", function () {
 });
 
 Then("I should be told {string}", function (expectedAnswer: string) {
-	assert.equal(this.actualAnswer, expectedAnswer);
+	assert.strictEqual(this.actualAnswer, expectedAnswer);
 });
