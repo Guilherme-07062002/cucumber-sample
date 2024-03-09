2763974c0d39ce022a8d58a6e4fdd7f7deffb17c
Guilherme-07062002 <guilherme762002@gmail.com>
2024-03-09 20:10:50 -0300

    test: adds a new scenario and step definition for checking if today is friday

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -9,6 +9,10 @@ const itIsFriday = (today: string) => {
 Given("today is Sunday", function () {
 	this.today = "Sunday";
 });
+
+Given("today is Friday", function () {
+	this.today = "Friday";
+});
          
 When("I ask whether it's Friday yet", function () {
 	this.actualAnswer = itIsFriday(this.today);
